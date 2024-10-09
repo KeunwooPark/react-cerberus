951a2842aef2577e1a24ff5ccdefbb1cb07ce398
keunwoo <keunwoopark.524@gmail.com>
2024-10-09 16:26:58 +0900

    init elements package

---
+++ packages/react-cerberus/elements/src/lib/elements.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledElements = styled.div`
+  color: pink;
+`;
+
+export function Elements() {
+  return (
+    <StyledElements>
+      <h1>Welcome to Elements!</h1>
+    </StyledElements>
+  );
+}
+
+export default Elements;
