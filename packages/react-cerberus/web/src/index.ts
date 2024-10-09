2b9f3b9ac879ae9bf4491eb9b169c9d7fa90421d
keunwoo <keunwoopark.524@gmail.com>
2024-10-09 21:27:57 +0900

    add publish workflow

--- packages/react-cerberus/web/src/index.ts
+++ packages/react-cerberus/web/src/index.ts
@@ -1,1 +1,15 @@
-export * from './renderer';
+import {
+  RendererPool,
+  setPlatform,
+  setRendererPool,
+} from '@react-cerberus/elements';
+import { DivRenderer } from './renderer';
+
+export function initReactCereberusWeb() {
+  const rendererPool: RendererPool = {
+    div: DivRenderer,
+  };
+
+  setRendererPool(rendererPool);
+  setPlatform('web');
+}
