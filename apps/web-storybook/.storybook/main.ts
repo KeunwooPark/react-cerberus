aadb6684b2667f5f07d5d8b58c014c26ef3cf456
keunwoo <keunwoopark.524@gmail.com>
2024-10-12 19:42:35 +0900

    set up storybook for web

--- apps/web-storybook/.storybook/main.ts
+++ apps/web-storybook/.storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: [
+    '../../../packages/react-cerberus-story/src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
---
+++ packages/react-cerberus-story/README.md
@@ -0,0 +1,7 @@
+# react-cerberus-story
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test react-cerberus-story` to execute the unit tests via [Jest](https://jestjs.io).
